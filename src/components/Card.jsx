cf965b6494d64bd9f101642fe836a0c311226358
Nicole-Villon-Alejandro <nicolevillon5@gmail.com>
2025-03-11 18:36:36 +0100

    add:icons and flags

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,33 @@
+import { GB, IT, JP, ES, FR, RO } from "country-flag-icons/react/3x2"
+
 export function Card ({data, type}) {
 
     const title = type === "movies" ? data.title : data.name
     const originalTitle = type === "movies" ? data.original_title : data.original_name
 
+    const language = data.original_language
+    let flag = null
+
+
+
+
 
     return (
         <>
-        <li>
+        <li className="card">
             <img src={`https://image.tmdb.org/t/p/w500${data.poster_path}`} alt={data.title} />
         </li>
-        <div>
+        <div className="card-info">
             <h2>{title}</h2>
             <p>
-                {originalTitle}
+                Original: {originalTitle}
             </p>
             <p>
                 ligua: {data.original_language}
 
             </p>
             <p>
-                Voto: {data.vote_average}
+                Voto: <span className="stars">{data.vote_average}</span>
 
             </p>
         </div>
