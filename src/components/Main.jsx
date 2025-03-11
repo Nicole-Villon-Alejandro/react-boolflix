0020ee7959a8243796df1175f3bd90e61c163aed
Nicole-Villon-Alejandro <nicolevillon5@gmail.com>
2025-03-11 16:16:16 +0100

    add:changes_vote

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,13 +4,15 @@ export function Main() {
 
   const { movies,  tvs} = useGlobalContext()
 
+  const HandleStars = (vote) => Math.cell(vote / 2)
+
     
   return (
     <main>
       <h1>Peliculas</h1>
       <ul>
         {movies.map((movie) => {
-          return <li key={movie.id}>{movie.title}</li>
+          return <li key={movie.id}>{movie.title}-{movie.vote_average}</li>
         })}
       </ul>
 
